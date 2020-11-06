78b5031bdc23f60a93278b487fa62bd499c4d49a
Hendrik Roth <hi@hendrikroth.com>
2020-11-06 10:24:22 +0100

    st patched, dwm config.h usage

--- modules/tiling.nix
+++ modules/tiling.nix
@@ -3,7 +3,6 @@
 {
     imports = [
       ../programs/dmenu
-      ../programs/dwm
       ../programs/st
     ];
 
@@ -12,10 +11,25 @@
         picom
         materia-theme
         dmenu
-        dwm
+        (dwm.overrideAttrs (oldAttrs: {
+          patches = [
+            ../programs/dwm/1-pertag.diff
+            ../programs/dwm/2-scratchpad.diff
+            ../programs/dwm/3-combo.diff
+            ../programs/dwm/4-centretitle.diff
+            ../programs/dwm/5-sticky.diff
+            ../programs/dwm/6-deck.diff
+            ../programs/dwm/7-columns.diff
+          ];
+          postPatch = let
+            configFile = pkgs.writeText "config.def.h" (builtins.readFile ../programs/dwm/config.h);
+          in
+            ''cp ${configFile} config.def.h'';
+        }))
         st
         xsel
     ];
+
 
     services = {
         xserver = {
--- programs/st/default.nix
+++ programs/st/default.nix
@@ -1,4 +1,11 @@
 {
   nixpkgs.config.st.patches = [
+    ./1-anysize.diff
+    ./2-scrollback.diff
+    ./3-scrollback-mouse.diff
+    ./4-xresources.diff
+    ./5-clipboard.diff
+    ./6-copyurl-highlight.diff
+    ./7-w3m.diff
   ];
 }
